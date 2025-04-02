74d4dd302c871770a21518dcf26961017d71fcd6
ventu-ra <ventura09.10.0@gmail.com>
2025-04-02 17:12:35 -0300

    update to angular 10

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,15 +2,10 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
 
 import { MEAT_API } from "app/app.api";
-import { Observable } from "rxjs";
-
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/catch";
+import { Observable, throwError } from "rxjs";
 
 import { Restaurant } from "./restaurant/restaurant.model";
-//import { ErrorHandler } from "app/app.error-handler";
 import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
-
 @Injectable()
 export class RestaurantsService {
   constructor(private http: HttpClient) {}
