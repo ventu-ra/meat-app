0a17f5cbdacd96e8036f4da351f53e0d04c58f14
ventu-ra <ventura09.10.0@gmail.com>
2025-04-15 08:34:11 -0300

    step 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, RouterModule, NavBarComponent],
+  template: ` <main class="main">
+      <header>
+        <app-nav-bar></app-nav-bar>
+      </header>
+      <section>
+        <router-outlet />
+      </section>
+    </main>
+    <footer class="lead py-5">
+      <div class="container">
+        <p class="m-0 text-center ">Copyright &copy; Meat 2025</p>
+      </div>
+    </footer>`,
   styleUrl: './app.component.css',
 })
 export class AppComponent {
