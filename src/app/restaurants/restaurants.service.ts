15ddeb9cc633b23f43d148281ac5d5f93a10304c
ventu-ra <ventura09.10.0@gmail.com>
2025-04-29 16:29:44 -0300

    Remove unused RxJS operators from restaurants.service.ts

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,9 +4,6 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 import { MEAT_API } from "app/app.api";
 import { Observable } from "rxjs";
 
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/catch";
-
 import { Restaurant } from "./restaurant/restaurant.model";
 //import { ErrorHandler } from "app/app.error-handler";
 import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
