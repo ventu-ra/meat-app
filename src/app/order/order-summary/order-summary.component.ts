5679744c347f78273588c9aebb75305f7f1e3414
ventu-ra <ventura09.10.0@gmail.com>
2025-05-03 18:27:03 -0300

    feat: add order summary component with rating functionality

--- src/app/order/order-summary/order-summary.component.ts
+++ src/app/order/order-summary/order-summary.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-import { RatingComponent } from "../shared/rating/rating.component";
+import { RatingComponent } from "../../shared/rating/rating.component";
 import { NgIf } from "@angular/common";
 
 @Component({
-    selector: "mt-order-summary",
-    templateUrl: "./order-summary.component.html",
-    imports: [RatingComponent, NgIf]
+  selector: "mt-order-summary",
+  templateUrl: "./order-summary.component.html",
+  imports: [RatingComponent, NgIf],
 })
 export class OrderSummaryComponent implements OnInit {
   rated: boolean;
