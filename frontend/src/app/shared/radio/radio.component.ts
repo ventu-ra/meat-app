39db259a5107c08ffec74185899fd61627cbe2cb
ventu-ra <ventura09.10.0@gmail.com>
2025-08-27 20:34:51 -0300

    style(radio): format code for consistency and readability

--- frontend/src/app/shared/radio/radio.component.ts
+++ frontend/src/app/shared/radio/radio.component.ts
@@ -4,31 +4,35 @@ import { RadioOption } from "./radio-option.model";
 import { NgFor } from "@angular/common";
 
 @Component({
-    selector: "mt-radio",
-    templateUrl: "./radio.component.html",
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => RadioComponent),
-            multi: true,
-        },
-    ],
-    imports: [NgFor]
+  selector: "mt-radio",
+  templateUrl: "./radio.component.html",
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true,
+    },
+  ],
+  imports: [NgFor]
 })
 export class RadioComponent implements OnInit, ControlValueAccessor {
   readonly options = input<RadioOption[]>(undefined);
 
   value: any;
 
-  onChange: any;
+  onChange: any = () => { };
+  onTouched: any = () => { };
+  isDisabled: boolean = false;
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   setValue(value: any) {
+    if (this.isDisabled) return;
     this.value = value;
     this.onChange(this.value);
+    this.onTouched();
   }
 
   writeValue(obj: any): void {
@@ -38,9 +42,9 @@ export class RadioComponent implements OnInit, ControlValueAccessor {
     this.onChange = fn;
   }
   registerOnTouched(fn: any): void {
- 
+    this.onTouched = fn;
   }
   setDisabledState?(isDisabled: boolean): void {
-
+    this.isDisabled = isDisabled;
   }
 }
