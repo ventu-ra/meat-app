229b8a5afc4d74fec573550a194b8b86852a5a37
ventu-ra <ventura09.10.0@gmail.com>
2025-08-28 14:20:19 +0000

    feat: add Vite dependencies for RxJS and SSR support
    - Introduced new package.json files for Vite dependencies with module type.
    - Added RxJS core functionality and operators as separate JavaScript files.
    - Included source maps for RxJS and its operators for better debugging.
    - Created metadata and package.json for SSR dependencies to enhance server-side rendering capabilities.

---
+++ frontend/.angular/cache/19.2.15/meat/vite/deps/@angular_platform-browser.js
@@ -0,0 +1,86 @@
+import {
+  By,
+  DomSanitizer,
+  DomSanitizerImpl,
+  HAMMER_GESTURE_CONFIG,
+  HAMMER_LOADER,
+  HammerGestureConfig,
+  HammerGesturesPlugin,
+  HammerModule,
+  HydrationFeatureKind,
+  Meta,
+  Title,
+  VERSION,
+  disableDebugTools,
+  enableDebugTools,
+  provideClientHydration,
+  withEventReplay,
+  withHttpTransferCacheOptions,
+  withI18nSupport,
+  withIncrementalHydration,
+  withNoHttpTransferCache
+} from "./chunk-GO33RFP6.js";
+import "./chunk-D6ZKYGQW.js";
+import {
+  BrowserDomAdapter,
+  BrowserGetTestability,
+  BrowserModule,
+  DomEventsPlugin,
+  DomRendererFactory2,
+  EVENT_MANAGER_PLUGINS,
+  EventManager,
+  EventManagerPlugin,
+  KeyEventsPlugin,
+  REMOVE_STYLES_ON_COMPONENT_DESTROY,
+  SharedStylesHost,
+  bootstrapApplication,
+  createApplication,
+  platformBrowser,
+  provideProtractorTestingSupport
+} from "./chunk-U4JAOFTQ.js";
+import {
+  getDOM
+} from "./chunk-X3ND3L4G.js";
+import "./chunk-LWUG2NKX.js";
+import "./chunk-QPOYJYPQ.js";
+import "./chunk-MZJUFL2B.js";
+import "./chunk-GLOCSHMG.js";
+import "./chunk-D77PI7IO.js";
+export {
+  BrowserModule,
+  By,
+  DomSanitizer,
+  EVENT_MANAGER_PLUGINS,
+  EventManager,
+  EventManagerPlugin,
+  HAMMER_GESTURE_CONFIG,
+  HAMMER_LOADER,
+  HammerGestureConfig,
+  HammerModule,
+  HydrationFeatureKind,
+  Meta,
+  REMOVE_STYLES_ON_COMPONENT_DESTROY,
+  Title,
+  VERSION,
+  bootstrapApplication,
+  createApplication,
+  disableDebugTools,
+  enableDebugTools,
+  platformBrowser,
+  provideClientHydration,
+  provideProtractorTestingSupport,
+  withEventReplay,
+  withHttpTransferCacheOptions,
+  withI18nSupport,
+  withIncrementalHydration,
+  withNoHttpTransferCache,
+  BrowserDomAdapter as ɵBrowserDomAdapter,
+  BrowserGetTestability as ɵBrowserGetTestability,
+  DomEventsPlugin as ɵDomEventsPlugin,
+  DomRendererFactory2 as ɵDomRendererFactory2,
+  DomSanitizerImpl as ɵDomSanitizerImpl,
+  HammerGesturesPlugin as ɵHammerGesturesPlugin,
+  KeyEventsPlugin as ɵKeyEventsPlugin,
+  SharedStylesHost as ɵSharedStylesHost,
+  getDOM as ɵgetDOM
+};
--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@
 # Executa o Spring Boot na pasta backend
 echo "Iniciando backend com Spring Boot..."
 (cd backend && ./mvnw spring-boot:run)
+(cd frontend && pnpm install && pnpm run start)
