4230531832b2a2fd53598b15b4fb5b44fcd3add7
ventu-ra <ventura09.10.0@gmail.com>
2025-08-29 20:52:16 +0000

    chore(import): remove unused menu insert statement
    * Deleted redundant INSERT statement for 'Cup Cake' in import.sql
    * Added readiness timeout configuration in application.properties

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -90,22 +90,6 @@ VALUES (
     );
 
 INSERT INTO
-    "tb_menus" (
-        "image_path",
-        "name",
-        "price",
-        "restaurant_id",
-        "description"
-    )
-VALUES (
-        'assets/img/foods/cupcake.png',
-        'Cup Cake',
-        8.7,
-        1,
-        'Cup Cake recheado de Doce de Leite'
-    );
-
-INSERT INTO
     tb_menus (
         image_path,
         name,
